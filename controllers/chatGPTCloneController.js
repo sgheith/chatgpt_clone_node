f7d895f55ea5fa72132ee3ae94a9c93f7a1df077
Shadi Ghaith <s_gheith@hotmail.com>
2023-08-10 14:58:36 +0300

    lesson-04

--- controllers/chatGPTCloneController.js
+++ controllers/chatGPTCloneController.js
@@ -1,17 +1,18 @@
 const openai = require('../config/openaiConfig')
 
-const generateChatCompletion = async (messages) => {
+const generateChatCompletion = async (req, res) => {
 
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: messages,
+    messages: req.body.messages,
     temperature: 0.7
   })
 
   //console.log(response.data.choices[0].message.content)
 
-  return response.data.choices[0].message.content
-
+  res.status(200).json({
+    content: response.data.choices[0].message.content
+  })
 }
 
 module.exports = { generateChatCompletion }
